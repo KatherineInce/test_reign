37dd3bf96dca051b49c58e4e4af220b09830af48
kince <katherine.ince0390@gmail.com>
2022-07-04 02:34:22 -0500

    adding logo and navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-
+import Logo from './components/Logo.js'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
+        <Logo/>
 
+        <Navbar/>
+        <section>
+
+        </section>
+        <footer>
+
+        </footer>
     </div>
   );
 }
---
+++ src/components/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <div className="header__logo">
+     <span className="header__logo__title">
+        HACKER NEWS
+     </span>
+    </div>
+  )
+}
+
+export default Logo
